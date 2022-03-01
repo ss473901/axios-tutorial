96d6055a0ebdea7399db9e4e27bf1cb746c6a3fe
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-03-01 21:04:57 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import jsonplaceholder from "./apis/jsonplaceholder";
+import Button from "./components/Button";
+import Resources from "./components/Resources";
+
+const App = () => {
+  const [resources, setResources] = useState([]);
+  const getPosts = async () => {
+    try {
+      const posts = await jsonplaceholder.get("/posts");
+      setResources(posts.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const getAlbums = async () => {
+    try {
+      const albums = await jsonplaceholder.get("/albums");
+      setResources(albums.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container" style={{ marginTop: "30px" }}>
+      <Button onClick={getPosts} color="primary" text="posts" />
+      <Button onClick={getAlbums} color="red" text="albums" />
+      <Resources resources={resources} />
     </div>
   );
-}
+};
 
 export default App;
